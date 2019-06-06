02e861aa7018e3ad37202b38db7ceae24464f46c
William Bergh <willebergh@outlook.com>
2019-06-07 01:23:40 +0200

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
 
 
 // Connect to MongoDB
@@ -8,6 +8,15 @@ const MongoDB = require("./database/MongoDB");
 const mongo = new MongoDB();
 mongo.load() ? mongo.connect(process.env.DB_URI) : console.log("[MongoDB]", "Failed to load.")
 
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3001",
+    methods: "GET, PUT, POST, DELETE, UPDATE, OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 201,
+    credentials: true,
+    allowedHeaders: "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+}));
 
 
 // Routes
