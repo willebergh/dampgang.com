326ac6219c745f7b04a73a52915ba1a725126bdd
willebergh <willebergh@outlook.com>
2019-06-06 11:48:55 +0200

    Added basic routing for the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Landing from "./components/views/Landing";
+import Members from "./components/views/Members";
+import Servers from "./components/views/Servers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/members" component={Members} />
+      <Route exact path="/servers" component={Servers} />
+    </Router>
   );
 }
 
