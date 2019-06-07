419cdca7ccf5560deb2e60de857bc690aa731f49
willebergh <willebergh@outlook.com>
2019-06-07 14:05:35 +0200

    added route for register view component

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Landing from "./components/views/Landing";
 import Login from "./components/views/Login";
+import Register from "./components/views/Register";
 import Members from "./components/views/Members";
 import Servers from "./components/views/Servers";
 
 function App() {
   return (
     <Router>
+
       <Route exact path="/" component={Landing} />
+
       <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+
       <Route exact path="/members" component={Members} />
       <Route exact path="/servers" component={Servers} />
+
     </Router>
   );
 }
