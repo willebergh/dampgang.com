a98951ea4491b277d7c2c7cc2c56d5674e376595
William Bergh <willebergh@outlook.com>
2019-06-10 22:37:47 +0200

    added action types

---
+++ client/src/actions/types.js
@@ -0,0 +1,15 @@
+// Auth action types
+export const CHECK_AUTH = "CHECK_AUTH"
+export const CHECKING_AUTH = "CHECKING_AUTH"
+export const CHECKED_AUTH = "CHECKED_AUTH"
+
+export const USER_LOADED = "USER_LOADED";
+export const USER_LOADING = "USER_LOADING";
+export const AUTH_ERROR = "AUTH_ERROR";
+
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_LOADING = "LOGIN_LOADING";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
